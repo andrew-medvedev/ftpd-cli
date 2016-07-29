878bed96f84e535f5cd9e03edd0d5131f9c83326
Andrew Medvedev <andrew.medvedev.nh@gmail.com>
2016-07-29 14:28:35 +0500

    initial

---
+++ README.md
@@ -0,0 +1,4 @@
+# Truly simple zero-config 5-sec FTP server in pure JavaScript
+
+It is based on this npm module: https://www.npmjs.com/package/ftpd
+
---
+++ index.js
@@ -0,0 +1,118 @@
+#!/usr/bin/env node
+'use strict';
+
+var ftpd = require('ftpd'),
+    _ = require('underscore');
+
+var DIRECTORY_ARG = '--directory',
+    DIRECTORY_ARG_SHORT = '-D',
+    PORT_ARG = '--port',
+    PORT_ARG_SHORT = '-P',
+    USER_PASSWORD_ARG = '--user-password',
+    USER_PASSWORD_ARG_SHORT = '-UP';
+
+function parseArguments(args){
+    var argsBook = { port: 21 };
+    
+    for(var i = 0 ; i < args.length ; i++){
+        switch(args[i]){
+            case DIRECTORY_ARG:
+            case DIRECTORY_ARG_SHORT:
+                if(++i < args.length){
+                    argsBook.directory = args[i];
+                }
+                break;
+            case PORT_ARG:
+            case PORT_ARG_SHORT:
+                if(++i < args.length){
+                    argsBook.port = parseInt(args[i]);
+                }
+                break;
+            case USER_PASSWORD_ARG:
+            case USER_PASSWORD_ARG_SHORT:
+                if(++i < args.length){
+                    argsBook.users = [];
+                    
+                    var usrs = args[i].split(',');
+                    _.each(usrs, function(u){
+                        var login = u.split(':')[0],
+                            password = u.split(':')[1];
+
+                        argsBook.users.push({
+                            login: login,
+                            password: password
+                        })
+                    });
+                }
+                break;
+        }
+    }
+
+    if(_.isUndefined(argsBook.directory)){
+        return console.log('Directory must be given');
+    }
+    if(_.isUndefined(argsBook.port) || _.isNaN(argsBook.port)){
+        return console.log('Port must be given and valid');
+    }
+
+    var server = new ftpd.FtpServer('0.0.0.0', {
+        getInitialCwd: function() {
+            return '/';
+        },
+        getRoot: function() {
+            return argsBook.directory;
+        },
+        useWriteFile: true,
+        useReadFile: true,
+        uploadMaxSlurpSize: 7000
+    });
+
+    server.on('error', function(error) {
+        console.log('FTP Server error:', error);
+    });
+
+    server.on('client:connected', function(connection) {
+        var username;
+        console.log('client connected: ' + connection.remoteAddress);
+        connection.on('command:user', function(user, success, failure) {
+            if(user === 'anonymous'){
+                if(_.isUndefined(argsBook.users) || argsBook.users.length === 0){
+                    username = user;
+                    success();
+                } else {
+                    failure();
+                }
+            } else {
+                if(_.isUndefined(argsBook.users) || argsBook.users.length === 0){
+                    failure();
+                } else {
+                    var userEntry = argsBook.users.find(function(e){ return e.login === user });
+                    if(_.isUndefined(userEntry)){
+                        failure();
+                    } else {
+                        username = userEntry;
+                        success();
+                    }
+                }
+            }
+        });
+
+        connection.on('command:pass', function(pass, success, failure) {
+            if(_.isUndefined(argsBook.users) || argsBook.users.length === 0){
+                success(username);
+            } else {
+                if(!_.isUndefined(username) && pass === username.password){
+                    success(username.login);
+                } else {
+                    failure();
+                }
+            }
+        });
+    });
+
+    server.debugging = 4;
+    server.listen(argsBook.port);
+    console.log('Listening on port ' + argsBook.port);
+}
+
+parseArguments(process.argv);
